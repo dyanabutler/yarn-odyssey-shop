e558e20b87a874995a1217a865948a2d1955ea91
Dyana Butler <dyanajbutler98@gmail.com>
2024-09-26 21:13:40 -0400

    redo

--- prisma/migrations/20240927011309_init/migration.sql
+++ prisma/migrations/20240927011309_init/migration.sql
@@ -27,8 +27,8 @@ CREATE TABLE "Order" (
     "updatedAt" DATETIME NOT NULL,
     "userId" TEXT NOT NULL,
     "productId" TEXT NOT NULL,
-    CONSTRAINT "Order_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT "Order_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+    CONSTRAINT "Order_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Order_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- CreateTable
